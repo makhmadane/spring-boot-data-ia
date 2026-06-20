0edabefe4e081568587397ace4c8b2f617799bdf
makhmadane <dl7781775@gmail.com>
2026-06-20 19:38:24 +0000

    CRUD ASSURANCE

--- src/main/java/sn/ia/gestion_assurance/controller/AssuranceController.java
+++ src/main/java/sn/ia/gestion_assurance/controller/AssuranceController.java
@@ -1,17 +1,56 @@
 package sn.ia.gestion_assurance.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import sn.ia.gestion_assurance.entity.Assurance;
+import sn.ia.gestion_assurance.repository.AssuranceRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/assurance")
 public class AssuranceController {
 
-    @GetMapping
-    public String getAllAssurance(){
-        System.out.println("Ici c Assurance");
-         return  "assurance/index";
+  /*  @Autowired  //IOC
+    private AssuranceRepository assuranceRepository;*/
+
+
+    //repository
+
+    //construct
+    private final AssuranceRepository assuranceRepository;
+
+   AssuranceController(AssuranceRepository assuranceRepository){
+        this.assuranceRepository = assuranceRepository;
     }
+
+    @GetMapping
+    public String getAllAssurance(Model model){
+        List<Assurance> assurances =  this.assuranceRepository.findAll();
+        model.addAttribute("assurances", assurances);
+        return  "assurance/index";
+    }
+
+    @GetMapping("/new")
+    public String createForm(Model model){
+       model.addAttribute("assurance",new Assurance());
+       return "assurance/add";
+    }
+
+    @PostMapping
+    public String save(@ModelAttribute Assurance assurance){
+       this.assuranceRepository.save(assurance);
+       return "redirect:/assurance";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Long id){
+        assuranceRepository.deleteById(id);
+        return "redirect:/assurance";
+    }
+
+
 }
